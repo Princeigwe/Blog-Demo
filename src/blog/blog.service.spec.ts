ba6d21f1ce35b6426405b7585bb5c50426e70027
Prince Igwe <igwep297@gmail.com>
2023-03-24 17:37:46 +0100

    mocked blog repo

--- src/blog/blog.service.spec.ts
+++ src/blog/blog.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BlogService } from './blog.service';
+import { Blog } from './blog.entity';
+import { getRepositoryToken } from "@nestjs/typeorm"
+
 
 describe('BlogService', () => {
   let service: BlogService;
 
+  function mockBlogRepository(dto:any) {
+    this.data = dto;
+    this.save  = () => { return this.data }
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BlogService],
+      providers: [
+        BlogService,
+        {
+          provide: getRepositoryToken(Blog),
+          useValue:mockBlogRepository
+        }
+      ],
     }).compile();
 
     service = module.get<BlogService>(BlogService);
