e0b2062f2da5a0ff6998b555228806a7c12aafb3
Prince Igwe <igwep297@gmail.com>
2023-03-24 18:03:20 +0100

    mocked service dependency

--- src/blog/blog.controller.spec.ts
+++ src/blog/blog.controller.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BlogController } from './blog.controller';
+import { BlogService } from './blog.service';
+
 
 describe('BlogController', () => {
   let controller: BlogController;
 
+  const mockBlogService = {}
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BlogController],
+      providers: [
+        { 
+          provide: BlogService, 
+          useValue: mockBlogService
+        }
+      ]
     }).compile();
 
     controller = module.get<BlogController>(BlogController);
